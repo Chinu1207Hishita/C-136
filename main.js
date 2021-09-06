1666fafd06fa70d5781d7edb66262e9206a37c4a
Hishita Bora <80160058+Chinu1207Hishita@users.noreply.github.com>
2021-09-06 17:51:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+video = "";
+status = "";
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup()
+canvas = createCanvas('480, 380');
+canvas.center();
+
